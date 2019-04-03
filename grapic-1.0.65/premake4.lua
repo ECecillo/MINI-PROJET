011b4d46fa8f3e40df5138c2925c2dfd7d9a6aeb
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2019-04-03 18:18:55 +0200

    ajout fichier

---
+++ grapic-1.0.65/premake4.lua
@@ -0,0 +1,10 @@
+
+-- Do not remove this line!
+dofile "./grapic.lua"
+
+
+-- To add a new project, it is here
+
+make_project( "LIFAP1_TP3", "apps/LIFAP1/main_LIFAP1_TP3.cpp" )
+-- make_project( "LIFAP1_TP7", "apps/LIFAP1/main_LIFAP1_TP7.cpp" )
+make_project( "LIFAMI_MINI-PROJET", "apps/MINI-PROJET/main_MINI-PROJET.cpp" )
